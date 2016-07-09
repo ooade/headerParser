c92ca05207484e731bc01132e41096657f99fc18
Adegbuyi Ademola <marhyorh@gmail.com>
2016-07-09 19:05:10 +0100

    Users can search for images and set offset :octocat:

--- routes/imageSearch.js
+++ routes/imageSearch.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
+/** We'll stick to ES5 since we're not transpiling to babel **/
+var BING_API_KEY = "902f50f9ce7d423289d56cad82c5ce50";
 
 /* Get imageSearch details */
-router.get('/', function(req, res, next) {
-  console.log("imagesearch read.y..");
-  // res.json({ ip_address, language, operating_system });
+router.get('/:searchQuery', function(req, res, next) {
+  var searchQuery = req.params.searchQuery;
+  var offset = req.query.offset;
+
+  var isOffset = offset ? `&offset=${offset}` : '';
+
+  axios.get(`https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${searchQuery}&count=10&${isOffset}`, {
+    headers: { 'Ocp-Apim-Subscription-Key': BING_API_KEY }
+  })
+    .then(function(response) {
+      // console.log("done...");
+      var array = response.data.value;
+
+      var result = array.map(function(data){
+        return {
+          image: data.contentUrl,
+          snippet: data.name,
+          pageUrl: data.hostPageUrl,
+          thumbnail: data.thumbnailUrl
+        };
+      });
+
+      res.json(result);
+    });
 });
 
 module.exports = router;
